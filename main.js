8f0fe2b0668797b9c7d6b3bc3c5d558f32636b61
Ioanna Fomina <fomina-ioanna@rambler.ru>
2026-03-18 21:39:45 +0300

    fix vh

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+  function setInitialViewportHeight() {
+    const h = window.innerHeight;
+    document.documentElement.style.setProperty('--initial-vh', `${h}px`);
+    document.documentElement.style.setProperty('--initial-vh-unit', `${h * 0.01}px`);
+  }
+
+  setInitialViewportHeight();
+
   const buttons = document.querySelectorAll('.js-toggle');
 
   const addressButton = document.querySelector('.js-address-button');
